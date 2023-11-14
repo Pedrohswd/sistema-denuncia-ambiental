77c7ceb4079a59ba0068dd77ef58c801d75c3c63
Pedro Henrique <pedrohsfwd@gmail.com>
2023-11-14 17:28:59 -0300

    criando o Projeto

---
+++ src/main/java/com/felas/ambieep/AmbieepApplication.java
@@ -0,0 +1,13 @@
+package com.felas.ambieep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmbieepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmbieepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felas/ambieep/AmbieepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felas.ambieep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmbieepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
