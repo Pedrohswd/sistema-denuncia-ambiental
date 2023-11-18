ccd8d3ce148c94b37958799022711edc5abb0edf
Pedrohswd <pedrohsfwd@gmail.com>
2023-11-18 18:41:00 -0300

    Ajuste no retorno de dados dos usuários

--- src/main/java/com/felas/ambieep/services/UserService.java
+++ src/main/java/com/felas/ambieep/services/UserService.java
@@ -28,7 +28,9 @@ public class UserService {
 
     public User buscarUsuarioPorId(Long id) {
         User user = userRepository.findById(id).orElse(null);
-        user.setPassword("");
+        if (user != null) {
+            user.setPassword("");
+        }
         return user;
     }
 
