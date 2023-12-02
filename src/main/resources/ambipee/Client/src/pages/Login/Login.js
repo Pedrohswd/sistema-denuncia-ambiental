c2b48fa596461d7e4cc27574e17a47b3c35d2885
educanprog <educandido.programador@gmail.com>
2023-12-02 17:16:32 -0300

    fixes front end

--- src/main/resources/ambipee/Client/src/pages/Login/Login.js
+++ src/main/resources/ambipee/Client/src/pages/Login/Login.js
@@ -29,9 +29,10 @@ function Login() {
         e.preventDefault();
     };
     const handleSubmit = async () => {
-            await api.post("http://localhost:8080/api/users/cpf", {
+            await api.post("/api/login/autenticate", {
             cpf: user.cpf,
             password: user.password
+
         }).then((response) => {
             if (response.data.status === true) {
                 accessToken = response.data.cpf;
@@ -55,46 +56,6 @@ function Login() {
     }; 
 
 
-    
-    /*
-    
-    const [passwordShown, setPasswordShown] = useState(false);
-    
-    const togglePasswordVisiblity = () => {
-        setPasswordShown(passwordShown ? false : true);
-    };
-     const handleInput = (e) => {
-        setUser({ ...user, [e.target.name]: e.target.value });
-    };
-    const preventSubmit = (e) => {
-        e.preventDefault();
-    };
-const handleSubmit = async () => {
-        await Axios.post(process.env.REACT_APP_CLIENT_LOGIN, {
-            cpf: user.cpf,
-            password: user.password
-        }).then((response) => {
-            if (response.data.status === true) {
-                accessToken = response.data.cpf;
-                permission = response.data.permission;
-                userName = response.data.userName;
-                cpf = response.data.cpf;
-                phone = response.data.phone;
-
-                localStorage.setItem('accessToken', accessToken);
-                localStorage.setItem('cpf', cpf);
-                localStorage.setItem('permission', permission);
-                localStorage.setItem('userName', userName);
-                localStorage.setItem('phone', phone);
-                navigate('/');
-            } else {
-                msg = response.data.msg;
-                if (msg === 'wrongPassword') alert('Senha incorreta. Verifique seus dados e tente novamente.');
-                if (msg === 'wrongCPF') alert('Usuário não encontrado. Verifique seus dados e tente novamente.');
-            }
-        })
-    }; */
-
     return (
         <div>
             <div className="loginMain">
@@ -104,11 +65,11 @@ const handleSubmit = async () => {
                     <h1>AMBIEEP</h1>
                     <form method="post" className='textbox'>
                         <p>CPF:</p>
-                        <input type="text" name="cpf" required />
+                        <input type="text" name="cpf" required onChange={handleInput}/>
                         <p>Senha:</p>
-                        <input name="password" required />
+                        <input name="password" required onChange={handleInput}/>
                         <br /><br />
-                        <button className='btn_entrar'>Entrar</button>
+                        <button className='btn_entrar' onSubmit={handleSubmit}>Entrar</button>
                     </form>
 
                 </div>
