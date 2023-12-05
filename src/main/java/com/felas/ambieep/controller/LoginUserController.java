46854cb86c1b5f38e2845eb49513cf0f1ac16f23
Pedro Henrique <pedrohsfwd@gmail.com>
2023-12-05 00:53:20 -0300

    ajustes login e cadastro de usuário

--- src/main/java/com/felas/ambieep/controller/LoginUserController.java
+++ src/main/java/com/felas/ambieep/controller/LoginUserController.java
@@ -1,10 +1,13 @@
 package com.felas.ambieep.controller;
 
 
+import com.felas.ambieep.entites.User;
 import com.felas.ambieep.entites.records.LoginJSON;
 import com.felas.ambieep.services.LoginUserService;
+import com.felas.ambieep.services.UserService;
 import com.felas.ambieep.utils.CPF;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,12 +17,16 @@ public class LoginUserController {
     @Autowired
     private LoginUserService loginUserService;
 
-    @PostMapping
-    public String loginUser(@RequestBody LoginJSON loginJSON){
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/log")
+    public ResponseEntity<User> loginUser(@RequestBody LoginJSON loginJSON){
         if(loginUserService.logonUser(loginJSON)){
-            return CPF.inserirMascara(loginJSON.cpf());
+            return ResponseEntity.ok(userService.findUserbyCPF(CPF.retirarMascara(loginJSON.cpf())));
+        } else{
+            return null;
         }
-        return "User not found";
     }
 
     @PostMapping("/autenticate")
